e28e34c50e0db735034201f8c77b69972d79725d
Muhamad Akmal <149946688+muhamadakmal1@users.noreply.github.com>
2025-11-25 14:39:05 +0530

    Set static API_URL in api.js
    Removed dynamic API_URL assignment and set a static URL.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://e-commerce-backend-0icu.onrender.com/api';
+// const API_URL = import.meta.env.VITE_API_URL || 'https://e-commerce-backend-0icu.onrender.com/api';
+
+const API_URL = 'https://e-commerce-backend-0icu.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
